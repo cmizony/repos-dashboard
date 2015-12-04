33542664d9ede62a6321d49ec88019138071849a
Camille Mizony <cmizony@gmail.com>
2015-12-03 20:28:19 -0800

    Add dashboard rendering mock data
    **Problem**
    It is difficult to know how to structure the metrics generated from the
    node modules without having a visual overview of how the data will be
    displayed.
    
    **Solution**
    Create a front-end dashboard which renders the metrics into a table
    generated using the Datatable library.
    
    The metrics object is stored as a javascript in the `repos-metrics.js`
    file. It is then being parsed into a flat array to be sent to the table.
    
    Some meta data are used to dynamically set some DOM element, like the
    period duration and the organization name.
    
    **Result**
    The beta version of the dashboard can be seen loading the `index.html`
    file into a browser. There is not require dependencies.

---
+++ dashboard/js/repos-metrics.js
@@ -0,0 +1,79 @@
+/**
+* @file Manually generated - Dec 2015
+*/
+var gloablMetrics = {
+  meta: {
+    organization: 'Docker',
+    periodStart: 'Jan 2015',
+    periodEnd: 'Mar 2015',
+    duration: '3 monthts'
+  },
+  data: [
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    },
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    },
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    },
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    },
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    },
+    {
+      repository:         'docker',
+      numberPR:           12,
+      descAvgPR:          50,
+      descAvgCommits:     15,
+      numberAvgCommits:   3,
+      numberAvgComments:  18,
+      noDescCommits:      1.12,
+      noDescPR:           22.32,
+      score:              800
+    }
+  ]
+}
