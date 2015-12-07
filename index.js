66abbc18d1ac6b6ec596ad5ddc5aa21ba8165235
Camille Mizony <cmizony@gmail.com>
2015-12-06 19:04:12 -0800

    Create and display repositories metrics
    **Problem**
    No metrics can be calculated out of the data cached from Github API
    
    **Solution**
    
    * Add metrics generation methods in module `repo-metrics.js`
    * Implement interface on `index.js` to output calculated metrics
    * Make dashboard read source file from `dashboard/metrics.json`
    * :memo: Update README to get started
    
    **Result**
    Metrics can now be generated and visualized on the dashboard

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-var github = require('./repo-loader.js')
-var github = require('./repo-metrics.js')
+var loader = require('./modules/repo-loader.js')
+var metrics = require('./modules/repo-metrics.js')
+var fs = require('fs')
 var Q = require('q')
 var _ = require('lodash')
 
 /**
 * @module Get Github organizations metrics
 * @author Camille Mizony
-* @TODO write interface prototypes
 * @version 1.0.0
 */
 module.exports = {
+  /**
+  * Create metrics for repositories of a given organization
+  * @method generateMetrics
+  * @public
+  * @param {String} organization
+  * @param {?Number} monthsHistory - optional
+  * @param {?String} outputFilePath - optional
+  * @returns {Object} promise - resolves to gloablMetrics
+  */
+  generateMetrics: function(organization, monthsHistory, outputFilePath) {
+    var deferred = Q.defer()
+    monthsHistory = monthsHistory || 3
+    outputFilePath = outputFilePath || 'dashboard/metrics.json'
+
+    loader.createCache(organization, monthsHistory).then(function(repositoriesDetails){
+      // 1. Generate meta object
+      var meta = {
+        organization: organization,
+        generated: new Date().toISOString(),
+        duration: monthsHistory + ' months'
+      }
+
+      // 2. Generate data array of metrics
+      var data =  _.reduce(repositoriesDetails, function(result, repositoryDetails) {
+        result.push(metrics.getRepositoryMetrics(repositoryDetails))
+        return result
+      }, [])
+
+      // 3. Write output file
+      var globalMetrics = {
+        meta: meta,
+        data: data
+      }
+      fs.writeFileSync(outputFilePath, JSON.stringify(globalMetrics, false, 2))
+
+      deferred.resolve(globalMetrics)
+    }).catch(function(error) { deferred.reject(new Error(error)) })
+
+    return deferred.promise
+  }
 }
